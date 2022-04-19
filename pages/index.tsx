56ccdee8ad6037ae05f235f1b04735a8929ee359
ireshka <ireshka@users.noreply.github.com>
2022-04-20 01:08:53 +0200

    feat: display <BoardWidget />

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import { Box, Container, Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { BoardWidget } from '../components/Board.widget';
+
+import * as React from 'react';
+
 
 const Home: NextPage = () => {
+
+
   return (
     <Container maxWidth="md">
-      <Typography variant="h3" component="h1" textAlign="center">
+      <Typography variant="h3" component="h1" textAlign="center" sx={{p: '2rem 2rem'}}>
         Game of Typescript Life
       </Typography>
-      <Box sx={{ width: "100%" }}></Box>
+      <Box sx={{ width: "100%" }}><BoardWidget /></Box>
     </Container>
   );
 };
